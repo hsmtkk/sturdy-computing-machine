c2c40ae29665bf4a3fa39f95cf6b7632ac45b207
hsmtkk <hsmtkk@gmail.com>
2023-12-20 13:26:51 +0900

    use new.css as stylesheet

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  app: {
+    head: {
+      link: [{ rel: 'stylesheet', href: 'https://fonts.xz.style/serve/inter.css' }, { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/@exampledev/new.css@1.1.2/new.min.css' }]
+    }
+  },
 })
