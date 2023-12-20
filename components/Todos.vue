5466d6dbef4c0d2757e045231ffaf8e37d4309e5
hsmtkk <hsmtkk@gmail.com>
2023-12-20 13:52:01 +0900

    useState to keep todos list

--- components/Todos.vue
+++ components/Todos.vue
@@ -1,3 +1,43 @@
 <template>
-    <p>Todos</p>
+    <h2>TODO List</h2>
+    <form v-on:submit.prevent="addClicked">
+        <input type="text" v-model="newTodo">
+        <button v-bind:disabled="newTodoEmpty">Add</button>
+    </form>
+    <ul>
+        <li v-for="(todo, index) in todos" v-bind:key="todo.task">
+            <input type="checkbox">
+            {{ todo.task }}
+            <button v-on:click="deleteClicked(index)">Delete</button>
+        </li>
+    </ul>
 </template>
+
+<script setup lang="ts">
+class Todo {
+    task: string
+    done: boolean
+    constructor(task: string) {
+        this.task = task
+        this.done = false
+    }
+}
+
+const todos = useState<Todo[]>("todos", (): Todo[] => {
+    return []
+})
+
+const newTodo: Ref<string> = ref("")
+
+const newTodoEmpty: Ref<boolean> = computed((): boolean => {
+    return newTodo.value === ""
+})
+
+const addClicked = () => {
+    todos.value.push(new Todo(newTodo.value))
+}
+
+const deleteClicked = (index: number) => {
+    todos.value.splice(index, 1)
+}
+</script>
